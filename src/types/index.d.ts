2c1a212fdcec8ce53ace389d14e26cdcbecacbc3
jingyang <3161362058@qq.com>
2024-05-23 22:57:57 +0800

    Init Template

---
+++ src/types/index.d.ts
@@ -0,0 +1,13 @@
+export type SiteConfig = {
+  name: string;
+  author: string;
+  description: string;
+  keywords: Array<string>;
+  url: {
+    base: string;
+    author: string;
+  };
+  links: {
+    github: string;
+  };
+};
