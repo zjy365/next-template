6dd5108fc7b8e836165a7da297175a8f9f55f06d
jingyang <3161362058@qq.com>
2024-05-24 10:27:23 +0800

    add husky lint-staged
    Signed-off-by: jingyang <3161362058@qq.com>

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,11 @@
 /** @type {import('prettier').Config} */
 module.exports = {
-  bracketSpacing: true,
-  semi: true,
-  trailingComma: "all",
-  printWidth: 80,
-  tabWidth: 2,
+  "bracketSpacing": true,
+  "endOfLine": "lf",
+  "printWidth": 80,
+  "semi": true,
+  "singleQuote": true,
+  "tabWidth": 2,
+  "trailingComma": "all",
   plugins: ["prettier-plugin-tailwindcss"],
 }
